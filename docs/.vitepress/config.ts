ef55082aadbeb1c2ff8043b12427b18dba477f88
creatorMao <zwmao@outlook.com>
2025-05-15 12:26:05 +0800

    ✨ feat: 增加文档功能

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "jswiss",
+  description: "像瑞士军刀一样的 JavaScript 工具",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
+    ],
+
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "Node环境", link: "/node" },
+          { text: "", link: "/api-examples" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
---
+++ docs/node.md
@@ -0,0 +1,13 @@
+---
+outline: deep
+---
+
+# 环境变量工具 envHelper
+
+```js
+import { envHelper } from "jswiss/node";
+
+envHelper.replaceEnvVar(envPath: string, map: Map<any, any>): void
+
+envHelper.getEnvVar(envPath: string, key: string): string;
+```
