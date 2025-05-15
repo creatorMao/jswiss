5f2303195e07d251f5818ebb26917ce7f83d346c
creatorMao <zwmao@outlook.com>
2025-05-15 13:15:59 +0800

    🔧 build: 设置base路径

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/jswiss/",
   title: "jswiss",
   description: "像瑞士军刀一样的 JavaScript 工具",
   themeConfig: {
